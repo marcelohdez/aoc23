c02f67ac0116cb0ca0fbcb968ee3974864e3104d
Marcelo Hernandez <marcelohdez.inq@gmail.com>
2023-12-07 14:08:04 -0500

    complete day 1
    needs cleanup

--- day-1/src/main.c
+++ day-1/src/main.c
@@ -1,40 +1,81 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <string.h>
+
+#define ARRLEN(arr) (sizeof(arr)/sizeof(*arr))
+
+const char* WORDS[] = {
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine"
+};
 
 int ctoi(char c) {
-  return c - 48; // where ascii numbers start (i don't like it but hey)
+  return c - '0'; // highly sophistaced ascii to integer conversion /s
 }
 
-int dissectline(char* str, size_t len) {
-  char first = 0;
-  char last = 0;
+// str is assumed null terminated (for strlen)
+int dissect(char* str) {
+  char* firstptr = NULL;
+  char* lastptr = NULL;
+  int first = 0;
+  int last = 0;
 
-  for (size_t i = 0; i < len; i++) {
-    char c = str[i];
-    if (c == '\n' || c == '\0') break;
+  for (int i = 0; i < ARRLEN(WORDS); i++) {
+    char* substrptr = strstr(str, WORDS[i]);
+    if (!substrptr) continue;
 
-    if (isdigit(c)) {
-      if (first == 0) first = c;
-      last = c;
+    while (substrptr) {
+      if (firstptr == NULL || substrptr < firstptr) {
+        firstptr = substrptr;
+        first = i;
+      }
+
+      if (lastptr == NULL || substrptr > lastptr) {
+        lastptr = substrptr;
+        last = i;
+      }
+
+      substrptr = strstr(substrptr + strlen(WORDS[i]), WORDS[i]);
     }
   }
-  if (first == 0) return 0;
 
-  return (ctoi(first) * 10) + ctoi(last);
+  for (int i = 0; i < strlen(str); i++) {
+    if (isdigit(str[i])) {
+      if (firstptr == NULL || &str[i] < firstptr) {
+        firstptr = &str[i];
+        first = ctoi(str[i]);
+      }
+
+      if (lastptr == NULL || &str[i] > lastptr) {
+        lastptr = &str[i];
+        last = ctoi(str[i]);
+      }
+    }
+  }
+
+  return (first * 10) + last;
 }
 
 int parse(FILE* fptr) {
   int total = 0;
 
   size_t len = 8; // default
-  char* str = malloc(8);
+  char* str = malloc(len);
 
   while (1) {
     getline(&str, &len, fptr);
     if (feof(fptr)) break;
 
-    total += dissectline(str, len);
+    total += dissect(str);
   }
 
   free(str);
